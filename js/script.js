b0af41f84ba7a4b2d5c819138eba2985ee7521bd
Darren Carlin <darrencarlin@Darrens-MBP.home>
2018-06-18 20:47:43 -0400

    day 2

--- js/script.js
+++ js/script.js
@@ -1,501 +1,135 @@
-const pokemon = [{
-    'name': 'bulbasaur',
-    'img': 'img/bulbasaur.png'
-}, {
-    'name': 'ivysaur',
-    'img': 'img/ivysaur.png'
-}, {
-    'name': 'venusaur',
-    'img': 'img/venusaur.png'
-}, {
-    'name': 'charmander',
-    'img': 'img/charmander.png'
-}, {
-    'name': 'charmeleon',
-    'img': 'img/charmeleon.png'
-}, {
-    'name': 'charizard',
-    'img': 'img/charizard.png'
-}, {
-    'name': 'squirtle',
-    'img': 'img/squirtle.png'
-}, {
-    'name': 'wartortle',
-    'img': 'img/wartortle.png'
-}, {
-    'name': 'blastoise',
-    'img': 'img/blastoise.png'
-}, {
-    'name': 'caterpie',
-    'img': 'img/caterpie.png'
-}, {
-    'name': 'metapod',
-    'img': 'img/metapod.png'
-}, {
-    'name': 'butterfree',
-    'img': 'img/butterfree.png'
-}, {
-    'name': 'weedle',
-    'img': 'img/weedle.png'
-}, {
-    'name': 'kakuna',
-    'img': 'img/kakuna.png'
-}, {
-    'name': 'beedrill',
-    'img': 'img/beedrill.png'
-}, {
-    'name': 'pidgey',
-    'img': 'img/pidgey.png'
-}, {
-    'name': 'pidgeotto',
-    'img': 'img/pidgeotto.png'
-}, {
-    'name': 'pidgeot',
-    'img': 'img/pidgeot.png'
-}, {
-    'name': 'rattata',
-    'img': 'img/rattata.png'
-}, {
-    'name': 'raticate',
-    'img': 'img/raticate.png'
-}, {
-    'name': 'spearow',
-    'img': 'img/spearow.png'
-}, {
-    'name': 'fearow',
-    'img': 'img/fearow.png'
-}, {
-    'name': 'ekans',
-    'img': 'img/ekans.png'
-}, {
-    'name': 'arbok',
-    'img': 'img/arbok.png'
-}, {
-    'name': 'pikachu',
-    'img': 'img/pikachu.png'
-}, {
-    'name': 'raichu',
-    'img': 'img/raichu.png'
-}, {
-    'name': 'sandshrew',
-    'img': 'img/sandshrew.png'
-}, {
-    'name': 'sandslash',
-    'img': 'img/sandslash.png'
-}, {
-    'name': 'nidoran-f',
-    'img': 'img/nidoran-f.png'
-}, {
-    'name': 'nidorina',
-    'img': 'img/nidorina.png'
-}, {
-    'name': 'nidoqueen',
-    'img': 'img/nidoqueen.png'
-}, {
-    'name': 'nidoran-m',
-    'img': 'img/nidoran-m.png'
-}, {
-    'name': 'nidorino',
-    'img': 'img/nidorino.png'
-}, {
-    'name': 'nidoking',
-    'img': 'img/nidoking.png'
-}, {
-    'name': 'clefairy',
-    'img': 'img/clefairy.png'
-}, {
-    'name': 'clefable',
-    'img': 'img/clefable.png'
-}, {
-    'name': 'vulpix',
-    'img': 'img/vulpix.png'
-}, {
-    'name': 'ninetales',
-    'img': 'img/ninetales.png'
-}, {
-    'name': 'jigglypuff',
-    'img': 'img/jigglypuff.png'
-}, {
-    'name': 'wigglytuff',
-    'img': 'img/wigglytuff.png'
-}, {
-    'name': 'zubat',
-    'img': 'img/zubat.png'
-}, {
-    'name': 'golbat',
-    'img': 'img/golbat.png'
-}, {
-    'name': 'oddish',
-    'img': 'img/oddish.png'
-}, {
-    'name': 'gloom',
-    'img': 'img/gloom.png'
-}, {
-    'name': 'vileplume',
-    'img': 'img/vileplume.png'
-}, {
-    'name': 'paras',
-    'img': 'img/paras.png'
-}, {
-    'name': 'parasect',
-    'img': 'img/parasect.png'
-}, {
-    'name': 'venonat',
-    'img': 'img/venonat.png'
-}, {
-    'name': 'venomoth',
-    'img': 'img/venomoth.png'
-}, {
-    'name': 'diglett',
-    'img': 'img/diglett.png'
-}, {
-    'name': 'dugtrio',
-    'img': 'img/dugtrio.png'
-}, {
-    'name': 'meowth',
-    'img': 'img/meowth.png'
-}, {
-    'name': 'persian',
-    'img': 'img/persian.png'
-}, {
-    'name': 'psyduck',
-    'img': 'img/psyduck.png'
-}, {
-    'name': 'golduck',
-    'img': 'img/golduck.png'
-}, {
-    'name': 'mankey',
-    'img': 'img/mankey.png'
-}, {
-    'name': 'primeape',
-    'img': 'img/primeape.png'
-}, {
-    'name': 'growlithe',
-    'img': 'img/growlithe.png'
-}, {
-    'name': 'arcanine',
-    'img': 'img/arcanine.png'
-}, {
-    'name': 'poliwag',
-    'img': 'img/poliwag.png'
-}, {
-    'name': 'poliwhirl',
-    'img': 'img/poliwhirl.png'
-}, {
-    'name': 'poliwrath',
-    'img': 'img/poliwrath.png'
-}, {
-    'name': 'abra',
-    'img': 'img/abra.png'
-}, {
-    'name': 'kadabra',
-    'img': 'img/kadabra.png'
-}, {
-    'name': 'alakazam',
-    'img': 'img/alakazam.png'
-}, {
-    'name': 'machop',
-    'img': 'img/machop.png'
-}, {
-    'name': 'machoke',
-    'img': 'img/machoke.png'
-}, {
-    'name': 'machamp',
-    'img': 'img/machamp.png'
-}, {
-    'name': 'bellsprout',
-    'img': 'img/bellsprout.png'
-}, {
-    'name': 'weepinbell',
-    'img': 'img/weepinbell.png'
-}, {
-    'name': 'victreebel',
-    'img': 'img/victreebel.png'
-}, {
-    'name': 'tentacool',
-    'img': 'img/tentacool.png'
-}, {
-    'name': 'tentacruel',
-    'img': 'img/tentacruel.png'
-}, {
-    'name': 'geodude',
-    'img': 'img/geodude.png'
-}, {
-    'name': 'graveler',
-    'img': 'img/graveler.png'
-}, {
-    'name': 'golem',
-    'img': 'img/golem.png'
-}, {
-    'name': 'ponyta',
-    'img': 'img/ponyta.png'
-}, {
-    'name': 'rapidash',
-    'img': 'img/rapidash.png'
-}, {
-    'name': 'slowpoke',
-    'img': 'img/slowpoke.png'
-}, {
-    'name': 'slowbro',
-    'img': 'img/slowbro.png'
-}, {
-    'name': 'magnemite',
-    'img': 'img/magnemite.png'
-}, {
-    'name': 'magneton',
-    'img': 'img/magneton.png'
-}, {
-    'name': 'farfetchd',
-    'img': 'img/farfetchd.png'
-}, {
-    'name': 'doduo',
-    'img': 'img/doduo.png'
-}, {
-    'name': 'dodrio',
-    'img': 'img/dodrio.png'
-}, {
-    'name': 'seel',
-    'img': 'img/seel.png'
-}, {
-    'name': 'dewgong',
-    'img': 'img/dewgong.png'
-}, {
-    'name': 'grimer',
-    'img': 'img/grimer.png'
-}, {
-    'name': 'muk',
-    'img': 'img/muk.png'
-}, {
-    'name': 'shellder',
-    'img': 'img/shellder.png'
-}, {
-    'name': 'cloyster',
-    'img': 'img/cloyster.png'
-}, {
-    'name': 'gastly',
-    'img': 'img/gastly.png'
-}, {
-    'name': 'haunter',
-    'img': 'img/haunter.png'
-}, {
-    'name': 'gengar',
-    'img': 'img/gengar.png'
-}, {
-    'name': 'onix',
-    'img': 'img/onix.png'
-}, {
-    'name': 'drowzee',
-    'img': 'img/drowzee.png'
-}, {
-    'name': 'hypno',
-    'img': 'img/hypno.png'
-}, {
-    'name': 'krabby',
-    'img': 'img/krabby.png'
-}, {
-    'name': 'kingler',
-    'img': 'img/kingler.png'
-}, {
-    'name': 'voltorb',
-    'img': 'img/voltorb.png'
-}, {
-    'name': 'electrode',
-    'img': 'img/electrode.png'
-}, {
-    'name': 'exeggcute',
-    'img': 'img/exeggcute.png'
-}, {
-    'name': 'exeggutor',
-    'img': 'img/exeggutor.png'
-}, {
-    'name': 'cubone',
-    'img': 'img/cubone.png'
-}, {
-    'name': 'marowak',
-    'img': 'img/marowak.png'
-}, {
-    'name': 'hitmonlee',
-    'img': 'img/hitmonlee.png'
-}, {
-    'name': 'hitmonchan',
-    'img': 'img/hitmonchan.png'
-}, {
-    'name': 'lickitung',
-    'img': 'img/lickitung.png'
-}, {
-    'name': 'koffing',
-    'img': 'img/koffing.png'
-}, {
-    'name': 'weezing',
-    'img': 'img/weezing.png'
-}, {
-    'name': 'rhyhorn',
-    'img': 'img/rhyhorn.png'
-}, {
-    'name': 'rhydon',
-    'img': 'img/rhydon.png'
-}, {
-    'name': 'chansey',
-    'img': 'img/chansey.png'
-}, {
-    'name': 'tangela',
-    'img': 'img/tangela.png'
-}, {
-    'name': 'kangaskhan',
-    'img': 'img/kangaskhan.png'
-}, {
-    'name': 'horsea',
-    'img': 'img/horsea.png'
-}, {
-    'name': 'seadra',
-    'img': 'img/seadra.png'
-}, {
-    'name': 'goldeen',
-    'img': 'img/goldeen.png'
-}, {
-    'name': 'seaking',
-    'img': 'img/seaking.png'
-}, {
-    'name': 'staryu',
-    'img': 'img/staryu.png'
-}, {
-    'name': 'starmie',
-    'img': 'img/starmie.png'
-}, {
-    'name': 'mr-mime',
-    'img': 'img/mr-mime.png'
-}, {
-    'name': 'scyther',
-    'img': 'img/scyther.png'
-}, {
-    'name': 'jynx',
-    'img': 'img/jynx.png'
-}, {
-    'name': 'electabuzz',
-    'img': 'img/electabuzz.png'
-}, {
-    'name': 'magmar',
-    'img': 'img/magmar.png'
-}, {
-    'name': 'pinsir',
-    'img': 'img/pinsir.png'
-}, {
-    'name': 'tauros',
-    'img': 'img/tauros.png'
-}, {
-    'name': 'magikarp',
-    'img': 'img/magikarp.png'
-}, {
-    'name': 'gyarados',
-    'img': 'img/gyarados.png'
-}, {
-    'name': 'lapras',
-    'img': 'img/lapras.png'
-}, {
-    'name': 'ditto',
-    'img': 'img/ditto.png'
-}, {
-    'name': 'eevee',
-    'img': 'img/eevee.png'
-}, {
-    'name': 'vaporeon',
-    'img': 'img/vaporeon.png'
-}, {
-    'name': 'jolteon',
-    'img': 'img/jolteon.png'
-}, {
-    'name': 'flareon',
-    'img': 'img/flareon.png'
-}, {
-    'name': 'porygon',
-    'img': 'img/porygon.png'
-}, {
-    'name': 'omanyte',
-    'img': 'img/omanyte.png'
-}, {
-    'name': 'omastar',
-    'img': 'img/omastar.png'
-}, {
-    'name': 'kabuto',
-    'img': 'img/kabuto.png'
-}, {
-    'name': 'kabutops',
-    'img': 'img/kabutops.png'
-}, {
-    'name': 'aerodactyl',
-    'img': 'img/aerodactyl.png'
-}, {
-    'name': 'snorlax',
-    'img': 'img/snorlax.png'
-}, {
-    'name': 'articuno',
-    'img': 'img/articuno.png'
-}, {
-    'name': 'zapdos',
-    'img': 'img/zapdos.png'
-}, {
-    'name': 'moltres',
-    'img': 'img/moltres.png'
-}, {
-    'name': 'dratini',
-    'img': 'img/dratini.png'
-}, {
-    'name': 'dragonair',
-    'img': 'img/dragonair.png'
-}, {
-    'name': 'dragonite',
-    'img': 'img/dragonite.png'
-}, {
-    'name': 'mewtwo',
-    'img': 'img/mewtwo.png'
-}, {
-    'name': 'mew',
-    'img': 'img/mew.png'
-}]
-
 let cardsArray = [];
-
-function populateCardsArray() {
-    for (let i = 0; i < 12; i++) {
-        const number = Math.floor((Math.random() * 151) + 1);
-        cardsArray.push({
-            name: `${pokemon[number].name}`,
-            img: `img/${pokemon[number].name}.png`
-        });
-        pokemon.slice(pokemon[number]);
-    }
-}
-populateCardsArray();
-let gameGrid = cardsArray.concat(cardsArray);
-gameGrid.sort(() => 0.5 - Math.random());
-const game = document.getElementById('game');
-const grid = document.createElement('section');
-grid.setAttribute('class', 'grid');
-game.appendChild(grid);
-gameGrid.forEach((item) => {
-    const card = document.createElement('div');
-    card.classList.add('card');
-    card.dataset.name = item.name;
-    const front = document.createElement('div');
-    front.classList.add('front');
-    const back = document.createElement('div');
-    back.classList.add('back');
-    back.style.backgroundImage = `url(${item.img})`;
-    grid.appendChild(card);
-    card.appendChild(front);
-    card.appendChild(back);
-});
 let firstGuess = '';
 let secondGuess = '';
 let count = 0;
+let counterTime = 0;
+let matches = 0;
+let guesses = 0;
+let delay = 1000;
 let previousTarget = null;
-let delay = 1200;
+let gameOver = false;
+let grid = document.getElementById('grid');
+let difficulty = document.getElementById('difficulty');
+let easy = false;
+let medium = true;
+let hard = false;
+let master = false;
+document.getElementById('guesses').innerHTML = guesses;
+document.getElementById('medium').style.backgroundColor = '#ffcb05';
+
+difficulty.addEventListener('click', () => {
+    let clicked = event.target.id;
+    if (clicked == 'easy') {
+        populateCardsArray(6)
+        easy = true;
+        medium = false;
+        hard = false;
+        master = false;
+        document.getElementById('easy').style.backgroundColor = '#ffcb05';
+        document.getElementById('medium').style.backgroundColor = 'initial';
+        document.getElementById('hard').style.backgroundColor = 'initial';
+        document.getElementById('master').style.backgroundColor = 'initial';
+    }
+    if (clicked == 'medium') {
+        populateCardsArray(12)
+        easy = false;
+        medium = true;
+        hard = false;
+        master = false;
+        document.getElementById('easy').style.backgroundColor = 'initial';
+        document.getElementById('medium').style.backgroundColor = '#ffcb05';
+        document.getElementById('hard').style.backgroundColor = 'initial';
+        document.getElementById('master').style.backgroundColor = 'initial';
+
+    }
+
+    if (clicked == 'hard') {
+        populateCardsArray(24)
+        easy = false;
+        medium = false;
+        hard = true;
+        master = false;
+        document.getElementById('easy').style.backgroundColor = 'initial';
+        document.getElementById('medium').style.backgroundColor = 'initial';
+        document.getElementById('hard').style.backgroundColor = '#ffcb05';
+        document.getElementById('master').style.backgroundColor = 'initial';
+        let cellArray = Array.from(document.querySelectorAll('.card,.front,.back'));
+        cellArray.forEach(function (node, idx) {
+            node.classList.add('hard-difficulty');
+        });
+    }
+    if (clicked == 'master') {
+        populateCardsArray(48, true)
+        easy = false;
+        medium = false;
+        hard = false;
+        master = true;
+        document.getElementById('easy').style.backgroundColor = 'initial';
+        document.getElementById('medium').style.backgroundColor = 'initial';
+        document.getElementById('hard').style.backgroundColor = 'initial';
+        document.getElementById('master').style.backgroundColor = '#ffcb05';
+        document.querySelector('.wrapper').style.maxWidth = '1100px';
+        let cellArray = Array.from(document.querySelectorAll('.card,.front,.back'));
+        cellArray.forEach(function (node, idx) {
+            node.classList.add('master-difficulty');
+        });
+    }
+})
+
+function populateCardsArray(diff = 12, master = false) {
+    let prev;
+    cardsArray = [];
+    for (let i = 0; i < diff; i++) {
+        if (master) {
+            cardsArray.push({
+                name: `${pokemon[i]['name']}`,
+                img: `img/${pokemon[i]['name']}.png`
+            });
+        } else {
+            var number = Math.floor((Math.random() * 151) + 0);
+            prev == number ? number = 0 : number;
+            cardsArray.push({
+                name: `${pokemon[number]['name']}`,
+                img: `img/${pokemon[number]['name']}.png`
+            });
+        }
+        prev = number;
+    }
+    let gameGrid = cardsArray.concat(cardsArray);
+    gameGrid.sort(() => 0.5 - Math.random());
+    populateGrid(gameGrid);
+}
+
+function populateGrid(gameGrid) {
+    grid.innerHTML = '';
+    gameGrid.forEach((item) => {
+        const card = document.createElement('div');
+        card.classList.add('card');
+        card.dataset.name = item.name;
+        const front = document.createElement('div');
+        front.classList.add('front');
+        const back = document.createElement('div');
+        back.classList.add('back');
+        back.style.backgroundImage = `url(${item.img})`;
+        grid.appendChild(card);
+        card.appendChild(front);
+        card.appendChild(back);
+    });
+}
+
 grid.addEventListener('click', () => {
     let clicked = event.target;
-    if (clicked.nodeName === 'SECTION' || clicked === previousTarget) {
+    if (counterTime === 0) {
+        start = setInterval(timer, 1000);
+    }
+    if (clicked.className === 'front') {
+        guesses++
+        document.getElementById('guesses').innerHTML = guesses;
+    }
+    if (clicked.nodeName === 'section' || clicked === previousTarget) {
         return;
     }
-    if (clicked.nodeName === 'SECTION') {
+    if (clicked.nodeName === 'section') {
         return;
     }
     if (count < 2) {
@@ -506,6 +140,7 @@ grid.addEventListener('click', () => {
         } else {
             secondGuess = clicked.parentNode.dataset.name;
             clicked.parentNode.classList.add('selected');
+            game.style.pointerEvents = "none";
         }
         if (firstGuess !== '' && secondGuess !== '') {
             if (firstGuess === secondGuess) {
@@ -518,13 +153,20 @@ grid.addEventListener('click', () => {
     }
     previousTarget = clicked;
 })
+
 const match = () => {
     var selected = document.querySelectorAll('.selected');
     selected.forEach(card => {
         card.classList.add('match');
     });
+    matches += 2;
+    if (matches === 12 && easy || matches === 24 && medium || matches === 48 && hard || matches === 96 && master) {
+        endGame();
+    }
 }
+
 const resetGuesses = () => {
+    game.style.pointerEvents = "initial";
     firstGuess = '';
     secondGuess = '';
     count = 0;
@@ -533,3 +175,28 @@ const resetGuesses = () => {
         card.classList.remove('selected');
     });
 };
+
+const endGame = () => {
+    gameOver = true;
+    clearInterval(start);
+    counterTime = false;
+    console.log('You Won');
+    console.log(document.getElementById('timer').innerHTML);
+    console.log(guesses);
+}
+
+const timer = () => {
+    counterTime++
+    let hours = Math.floor(counterTime / 3600);
+    let minutes = Math.floor((counterTime % 3600) / 60);
+    let seconds = counterTime % 60;
+    var timerOutput = '';
+    if (hours > 0) {
+        timerOutput += '' + hours + ':' + (minutes < 10 ? '0' : '');
+    }
+    timerOutput += '' + minutes + ':' + (seconds < 10 ? '0' : '');
+    timerOutput += '' + seconds;
+    document.getElementById('timer').innerHTML = timerOutput;
+}
+
+populateCardsArray();
